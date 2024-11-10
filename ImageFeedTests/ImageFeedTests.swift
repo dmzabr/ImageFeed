e26ffc017d380f5b91084887a6c8af3e0878d689
Dmitriy Zabrodskiy <dzabrik@yandex.ru>
2024-11-10 14:39:20 +0300

    Initial Commit

---
+++ ImageFeedTests/ImageFeedTests.swift
@@ -0,0 +1,17 @@
+//
+//  ImageFeedTests.swift
+//  ImageFeedTests
+//
+//  Created by  Дмитрий on 10.11.2024.
+//
+
+import Testing
+@testable import ImageFeed
+
+struct ImageFeedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
