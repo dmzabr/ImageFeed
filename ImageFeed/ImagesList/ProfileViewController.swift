2287b4aee59632093b0b416d279f9da71f8a29d8
dmitriy <dmitriy@dmzabr.ru>
2024-12-03 12:36:20 +0300

    Сверстан ProfileView

---
+++ ImageFeed/ImagesList/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by  Дмитрий on 30.11.2024.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
