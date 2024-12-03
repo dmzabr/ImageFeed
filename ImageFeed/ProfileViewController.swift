0609514ba46d6295b21983e8870dd94a445de720
dmitriy <dmitriy@dmzabr.ru>
2024-12-03 12:50:10 +0300

    Добавлены аутлеты

---
+++ ImageFeed/ProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by  Дмитрий on 30.11.2024.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLable: UILabel!
+    @IBOutlet weak var usernameLable: UILabel!
+    @IBOutlet weak var logOutButton: UIButton!
+    
+    @IBAction func didTapLogOutButton(_ sender: Any) {
+    }
+}
